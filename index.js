fbf15b738dc792076807b592aa6f45f587dcde49
Andy Stanberry <andystanberry@gmail.com>
2025-12-10 09:25:54 +0000

    port updateExtensionAuthenticationRequest

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const ExtensionsApi = require('./src/extensionsApi');
 const TenantsApi = require('./src/tenantsApi');
 const ServiceClientTokenProvider = require('./src/serviceClientTokenProvider');
 const TokenVerifier = require('./src/tokenVerifier');
+const jwtManager = require('./src/jwtManager');
 
 class AuthressClient {
   constructor(settings, tokenProvider) {
@@ -41,6 +42,50 @@ class AuthressClient {
 
   verifyToken(token) {
     return TokenVerifier(this.httpClient, token);
+  }
+
+  /**
+   * @description When a platform extension attempts to log a user in, the Authress Login page will redirect to your Platform defaultAuthenticationUrl. At this point, show the user the login screen, and then pass the results of the login to this method.
+   * @param {String} [state] The redirect to your login screen will contain two query parameters `state` and `flow`. Pass the state into this method.
+   * @param {String} [connectionId] Specify which provider connection that user would like to use to log in - see https://authress.io/app/#/manage?focus=connections
+   * @param {String} [tenantLookupIdentifier] Instead of connectionId, specify the tenant lookup identifier to log the user with the mapped tenant - see https://authress.io/app/#/manage?focus=tenants
+   * @param {String} [hint] Instead of connectionId or tenant lookup identifier, specify the user's domain or the full email for the user to dynamically identify and log the user with the mapped tenant.
+   * @param {Object} [connectionProperties] Connection specific properties to pass to the identity provider. Can be used to override default scopes for example.
+   * @return {Promise<AuthenticateResponse>} The authentication response.
+   */
+  async updateExtensionAuthenticationRequest({ state, connectionId, tenantLookupIdentifier, connectionProperties, hint }) {
+    if (!connectionId && !tenantLookupIdentifier && !hint) {
+      const e = Error('connectionId or tenantLookupIdentifier must be specified');
+      e.code = 'InvalidConnection';
+      throw e;
+    }
+
+    const authenticationRequestId = state;
+    if (!authenticationRequestId) {
+      const e = Error('The `state` parameters must be specified to update this authentication request');
+      e.code = 'InvalidAuthenticationRequest';
+      throw e;
+    }
+
+    try {
+      const resolvedTenantLookupIdentifier = hint || tenantLookupIdentifier;
+      const antiAbuseHash = await jwtManager.calculateAntiAbuseHash({ connectionId, tenantLookupIdentifier: resolvedTenantLookupIdentifier, authenticationRequestId });
+      const requestOptions = await this.httpClient.patch(`/api/authentication/${authenticationRequestId}`, {
+        antiAbuseHash,
+        connectionId,
+        tenantLookupIdentifier: resolvedTenantLookupIdentifier,
+        connectionProperties
+      });
+
+      return requestOptions.data;
+    } catch (error) {
+      if (error.status && error.status >= 400 && error.status < 500) {
+        const e = Error(error.data && (error.data.title || error.data.errorCode) || error.data || 'Unknown Error');
+        e.code = error.data && error.data.errorCode;
+        throw e;
+      }
+      throw (error.data || error);
+    }
   }
 }
 
--- src/jwtManager.js
+++ src/jwtManager.js
@@ -1,4 +1,5 @@
 const base64url = require('base64url');
+const crypto = require('crypto');
 
 class JwtManager {
   decode(token) {
@@ -7,6 +8,22 @@ class JwtManager {
     } catch (error) {
       return null;
     }
+  }
+
+  async calculateAntiAbuseHash(props) {
+    const timestamp = Date.now();
+    const valueString = Object.values(props).filter(v => v).join('|');
+
+    let fineTuner = 0;
+    let hash = null;
+    while (++fineTuner) {
+      hash = base64url.encode(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(`${timestamp};${fineTuner};${valueString}`)));
+      if (hash.match(/^00/)) {
+        break;
+      }
+    }
+
+    return `v2;${timestamp};${fineTuner};${hash}`;
   }
 }
 
